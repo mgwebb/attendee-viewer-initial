6a24ecd13bae7e955dd80934afbbc48a8aa48c69
mgwebb <mark.webb@cgi.com>
2017-06-07 14:02:30 -0600

    update config service reference in bootstrap.yml

--- src/main/java/io/rscale/training/attendeeviewer/CompanyClient.java
+++ src/main/java/io/rscale/training/attendeeviewer/CompanyClient.java
@@ -1,7 +1,13 @@
 package io.rscale.training.attendeeviewer;
 
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "roster")
 public interface CompanyClient {
 
+	@RequestMapping(method = RequestMethod.GET, value = "/companies")
 	Company getCompany(String id);
 	
 }
